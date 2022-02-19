f820413dabd03f472b7821ddad2650e4bb62b45b
Najwa <syibanajwa@gmail.com>
2022-02-20 00:28:46 +0700

    finish migrations

---
+++ database/migrations/2022_02_19_170318_create_camp_benefits_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampBenefitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+            // Metode 1
+            // $table->bigInteger('camp_id')->unsigned();
+            // $table->unsignedBigInteger('camp_id');
+            $table->string('name');
+            $table->timestamps();
+
+            // Metode 2
+            $table->foreignId('camp_id')->constrained();
+
+            // $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+}
